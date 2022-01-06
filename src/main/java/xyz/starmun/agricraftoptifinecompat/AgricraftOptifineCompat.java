08fad04fc899cfc94c62418314bac944c79a5b46
starmun <abdemuqsit@hotmail.com>
2022-01-06 21:21:13 +0500

    Fixed client-server compatibility check

--- src/main/java/xyz/starmun/agricraftoptifinecompat/AgricraftOptifineCompat.java
+++ src/main/java/xyz/starmun/agricraftoptifinecompat/AgricraftOptifineCompat.java
@@ -1,6 +1,10 @@
 package xyz.starmun.agricraftoptifinecompat;
 
+import net.minecraftforge.fml.ExtensionPoint;
+import net.minecraftforge.fml.ModLoadingContext;
 import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.network.FMLNetworkConstants;
+import org.apache.commons.lang3.tuple.Pair;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
@@ -8,10 +12,11 @@ import static xyz.starmun.agricraftoptifinecompat.AgricraftOptifineCompat.MOD_ID
 
 // The value here should match an entry in the META-INF/mods.toml file
 @Mod(MOD_ID)
-public class AgricraftOptifineCompat
-{
-    // Directly reference a log4j logger.
-    public static final Logger LOGGER = LogManager.getLogger();
-    public static final String MOD_ID= "agricraftoptifinecompat";
+public class AgricraftOptifineCompat {
+    public static final Logger LOGGER = LogManager.getLogger("Agricraft Optifine Compat");
+    public static final String MOD_ID = "agricraftoptifinecompat";
 
+    public AgricraftOptifineCompat() {
+        ModLoadingContext.get().registerExtensionPoint(ExtensionPoint.DISPLAYTEST, () -> Pair.of(() -> FMLNetworkConstants.IGNORESERVERONLY, (a, b) -> true));
+    }
 }
